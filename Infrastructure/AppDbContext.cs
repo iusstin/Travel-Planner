d935eb099ddf5fc5e5c17e353ed222979c5973ea
Iustin Deaconu <iustin.deaconu@globallogic.com>
2024-03-24 20:08:26 +0200

    Database connection User entity created

---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure;
+
+public class AppDbContext: IdentityDbContext<User>, IAppDbContext
+{
+    private readonly IConfiguration _config;
+    public AppDbContext(DbContextOptions<AppDbContext> options, IConfiguration config) : base(options)
+    {
+        _config = config;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        builder.UseSqlServer(_config.GetConnectionString("AppDbContext"));
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await base.SaveChangesAsync();
+    }
+
+    public DbSet<User> Users { get; set; }
+}
--- Travel-Planner.API/Program.cs
+++ Travel-Planner.API/Program.cs
@@ -1,11 +1,31 @@
+using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connection = builder.Configuration.GetConnectionString("AppDbContext");
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlServer(connection, builder => builder.EnableRetryOnFailure()));
+builder.Services.AddIdentityCore<User>().AddRoles<IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddScoped<IAppDbContext>(provider => provider.GetRequiredService<AppDbContext>());
+
+
+builder.Services.AddAutoMapper(Assembly.GetAssembly(typeof(AutoMapperProfile)));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    //.AddApplicationCore()
+    .AddInfrastructure();
 
 var app = builder.Build();
 
