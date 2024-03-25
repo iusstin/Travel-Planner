5d73d8820d6ea52c54c1dbb6470e3ef136a1db3e
Iustin Deaconu <iustin.deaconu@globallogic.com>
2024-03-25 18:18:17 +0200

    Get user by id command

---
+++ ApplicationCore/User/Queries/GetUserById/GetUserByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using ApplicationCore.BaseClasses;
+using System.Runtime.Serialization;
+
+namespace ApplicationCore.User.Queries.GetUserById;
+
+[DataContract]
+public class GetUserByIdQuery : BaseQuery<Domain.Entities.User?>
+{
+    [DataMember]
+    public long Id { get; set; }
+}
---
+++ ApplicationCore/User/Queries/GetUserById/GetUserByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationCore.BaseClasses;
+using Domain.Interfaces;
+using MediatR;
+
+namespace ApplicationCore.User.Queries.GetUserById;
+
+public class GetUserByIdQueryHandler : BaseQueryHandler<GetUserByIdQuery, Domain.Entities.User?>
+{
+    private readonly IUserRepository _userRepository;
+    public GetUserByIdQueryHandler(IMediator mediator, IUserRepository userRepository)
+        : base(mediator)
+    {
+        _userRepository = userRepository;
+    }
+
+    public override async Task<Domain.Entities.User?> Handle(GetUserByIdQuery cmd, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetById(cmd.Id.ToString());
+        return user;
+    }
+}
