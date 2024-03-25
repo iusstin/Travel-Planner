2c77f9b281313f80f8b1093e17255aae8ceca07e
Iustin Deaconu <iustin.deaconu@globallogic.com>
2024-03-25 13:04:39 +0200

    Initial migration

---
+++ Domain/Interfaces/IAppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Interfaces;
+
+public interface IAppDbContext
+{
+    public DbSet<User> Users { get; set; }
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
